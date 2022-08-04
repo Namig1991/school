1e41722b14dca676c3efb7f5deada7b902ceba35
Namig1991 <safarovnamig09@gmail.com>
2022-08-04 19:04:48 +0300

    Мой первый коммит по Д/З № 3, четвертый курс.

---
+++ src/main/resources/liquibase/scripts/index-practice.sql
@@ -0,0 +1,5 @@
+--liquibase formatted sql
+--changeset nsafarov:1
+CREATE INDEX student_name_index ON student (name);
+-- changeset nsafarov:2
+CREATE INDEX faculty_name_and_color_index ON faculty (name, color);
